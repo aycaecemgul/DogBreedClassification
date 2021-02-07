b3e799f226d368630af95baaa5ec1264da5179ad
aycaecemgul <aycaecemgul@gmail.com>
2021-02-07 16:26:21 +0300

    test

--- main.py
+++ main.py
@@ -50,51 +50,51 @@ def count_images(DIR):
 
     print("amount of images= "+str(i))
 
-training_data=[]
-
-for category in os.listdir(DIR):
-    path = os.path.join(DIR, category)
-    for img in os.listdir(path):
-        class_no = os.listdir(DIR).index(category)
-        filename = os.path.join(path, img)
-        img_array = asarray(skimage.io.imread(filename))
-        training_data.append([img_array, class_no])
-
-print("training data is loaded.")
-
-#data augmentation
-for category in os.listdir(DIR):
-    path = os.path.join(DIR, category)
-    for img in os.listdir(path):
-        class_no = os.listdir(DIR).index(category)
-        filename = os.path.join(path, img)
-        img_array = asarray(skimage.io.imread(filename))
-        training_data.append([rotate(img_array, angle=30, resize=False), class_no])
-        training_data.append([cv.flip(img_array, 1), class_no])
-
-print("Data augmentation is done.")
-
-X=[]
-y=[]
-
-for image,label in training_data:
-    X.append(image)
-    y.append(label)
-
-X=np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,3)/255.0
-y = np.array(y)
-
-print(X.shape)
-
-pickle_out = open("X.pickle","wb")
-pickle.dump(X, pickle_out)
-pickle_out.close()
-
-pickle_out = open("y.pickle","wb")
-pickle.dump(y, pickle_out)
-pickle_out.close()
-
-print("Pickled.")
+# training_data=[]
+#
+# for category in os.listdir(DIR):
+#     path = os.path.join(DIR, category)
+#     for img in os.listdir(path):
+#         class_no = os.listdir(DIR).index(category)
+#         filename = os.path.join(path, img)
+#         img_array = asarray(skimage.io.imread(filename))
+#         training_data.append([img_array, class_no])
+#
+# print("training data is loaded.")
+#
+# #data augmentation
+# for category in os.listdir(DIR):
+#     path = os.path.join(DIR, category)
+#     for img in os.listdir(path):
+#         class_no = os.listdir(DIR).index(category)
+#         filename = os.path.join(path, img)
+#         img_array = asarray(skimage.io.imread(filename))
+#         training_data.append([rotate(img_array, angle=30, resize=False), class_no])
+#         training_data.append([cv.flip(img_array, 1), class_no])
+#
+# print("Data augmentation is done.")
+#
+# X=[]
+# y=[]
+#
+# for image,label in training_data:
+#     X.append(image)
+#     y.append(label)
+#
+# X=np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,3)/255.0
+# y = np.array(y)
+#
+# print(X.shape)
+#
+# pickle_out = open("X.pickle","wb")
+# pickle.dump(X, pickle_out)
+# pickle_out.close()
+#
+# pickle_out = open("y.pickle","wb")
+# pickle.dump(y, pickle_out)
+# pickle_out.close()
+#
+# print("Pickled.")
 
 pickle_in = open("X.pickle","rb")
 X = pickle.load(pickle_in)
@@ -105,8 +105,17 @@ y = pickle.load(pickle_in)
 print("Pickle jar opened.")
 
 print(X.shape)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,shuffle= True)
 X_train, X_val, y_train, y_val  = train_test_split(X_train, y_train, test_size=0.33, shuffle= True )
+
+pickle_out = open("X_test.pickle","wb")
+pickle.dump(X_test, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y_test.pickle","wb")
+pickle.dump(y_test, pickle_out)
+pickle_out.close()
 
 base_model = InceptionV3(
                                 weights='imagenet',
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 from tensorflow.keras import models
 import numpy as np
 import pickle
+from matplotlib import pyplot as plt
 
+IMG_SIZE=120
 CATEGORIES=["Afghan Hound","Beagle","Blenheim Spaniel","Japanese Spaniel","Maltese Terrier",
             "Papillon","Pom","Rhodesian Ridgeback","Samoyed","Shih-Tzu"]
 
@@ -16,11 +18,16 @@ y_test = pickle.load(pickle_in)
 
 predict=model.predict([X_test])
 
+t=1
 
-p=np.argmax(predict[15])
+p=np.argmax(predict[t])
 
-q=y_test[15]
+q=y_test[t]
 
 print("Actual breed of the dog: "+str(CATEGORIES[q]))
 print("The prediction of the model: "+str(CATEGORIES[p]))
 
+X_test[t]=X_test[t].reshape(IMG_SIZE,IMG_SIZE,3)*255.0
+
+plt.imshow(X_test[t])
+plt.show()
